01aadb2f4b9336634641f98d94e86168e1e09ace
gaurikakkr <111335758+gaurikakkr@users.noreply.github.com>
2023-11-21 22:27:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongose = require('mongoose');
+const {Product} = require('./models/product');
+require('dotenv/config');
+
+const api = process.env.API_URL;
+const productsRouter = require('./routers/product')
+//midleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+//routers
+app.use(`${api}/products`, productsRouter);
+
+
+
+//Database
+mongose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
+// app.get(`${api}/products`,async (req,res) =>{
+//     const productList = await Product.find();
+
+//     if(!productList){
+//         res.status(500).json({success: false})
+//     }
+//     res.send(productList);
+// })
+// app.post(`${api}/products`, (req,res)=>{
+//     const product = new Product({
+//         name: req.body.name,
+//         image: req.body.image,
+//         countInStock: req.body.countInStock
+//     })
+
+//     product.save().then((createdProduct=>{
+//         res.status(201).json(createdProduct)
+//     })).catch((err)=>{
+//         res.status(500).json({
+//             error: err,
+//             success: false
+//         })
+//     })
+// })
+
+app.listen(3000, ()=>{
+
+    console.log('server is running http://localhost:3000');
+})
